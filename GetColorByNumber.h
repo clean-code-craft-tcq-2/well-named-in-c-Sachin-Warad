cd9467e0e5de2fcc289ead92cdb6336a63c1789d
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-01-15 23:43:46 +0530

    Update GetColorByNumber.h

--- GetColorByNumber.h
+++ GetColorByNumber.h
@@ -1,4 +1,5 @@
 extern int numberOfMinorColors;
+extern int numberOfMajorColors;
 
 ColorPair GetColorFromPairNumber(int pairNumber);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
