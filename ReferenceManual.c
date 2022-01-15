72f8c288f67eab3cbf4364bab166f6660cf5c6bc
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-01-16 00:22:21 +0530

    Update ReferenceManual.c

--- ReferenceManual.c
+++ ReferenceManual.c
@@ -1,3 +1,16 @@
 #include "ReferenceManual.h"
 #include "main.h"
 #include "GetColorByNumber.h"
+
+void PrintManual()
+{
+    printf("Reference Manual for Color Coding\n\n");
+    int PairNumber;
+    for(PairNumber=1; PairNumber <= 25; PairNumber++)
+    {
+        ColorPair colors = GetColorFromPairNumber(PairNumber);
+        char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+        ColorPairToString(&colors, colorPairNames);
+        printf("Color Pair:%d\t\t Major Minor Color Pair:%s\n",PairNumber,colorPairNames );
+    }
+}
