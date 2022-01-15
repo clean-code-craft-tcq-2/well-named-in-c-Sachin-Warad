db5592d8fd3fd8a1e8af64ed21c5daeb3add0f6c
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-01-15 21:35:41 +0530

    Initial Version of GetNumberByColor.c

---
+++ GetNumberByColor.c
@@ -0,0 +1,6 @@
+#include "GetNumberByColor.h"
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
