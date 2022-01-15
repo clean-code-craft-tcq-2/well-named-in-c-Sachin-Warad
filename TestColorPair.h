22b2b1db52e73ad12eb1b25f7a8ce5f6a6cd6931
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-01-15 23:12:40 +0530

    Update TestColorPair.h

--- TestColorPair.h
+++ TestColorPair.h
@@ -1,5 +1,7 @@
 #include <assert.h>
 
+#define MAX_COLORPAIR_NAME_CHARS  16
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
