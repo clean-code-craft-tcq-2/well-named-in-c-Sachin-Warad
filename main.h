e536d9e52f45d1daeba9c4efa931bfd27f530991
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-01-15 23:06:51 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,25 +1,9 @@
 #include <stdio.h>
 
-#ifndef _MAIN_H
-#define _MAIN_H
-
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
 
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
-
-extern int numberOfMajorColors;
-extern int numberOfMinorColors;
-
-#endif
