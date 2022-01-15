856b64a8fbc76ba72ea98c5af9c4f2f768353278
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-01-15 23:09:08 +0530

    Update GetColorByNumber.c

--- GetColorByNumber.c
+++ GetColorByNumber.c
@@ -1,6 +1,20 @@
 #include "main.h"
 #include "GetColorByNumber.h"
 
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
